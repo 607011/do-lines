430129a916ad374d8dc618db336b1fa7b44f1335
Oliver Lau <oliver@ersatzworld.net>
2022-05-07 08:11:03 +0200

    Deployment now via rsync.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# .env must define the following variables:
+# HOST: the SSH target of the host to deploy to, e.g. username@www.example.com
+# DEST_DIR: the path to the directory on the host where to copy the files to
+
 source .env
-exiftool -all= chalkboard-3084w.png
-scp index.html default.css bart.js chalkboard-3840w.png GochiHand-Regular.ttf olau@${HOST}:${DEST_DIR}
+
+DIST=dist
+mkdir -p ${DIST}
+
+echo "Collecting ..."
+while read filename; do
+    echo ${filename}
+    dir=$(dirname ${filename})
+    mkdir -p ${DIST}/${dir}
+    cp ${filename} ${DIST}/${filename}
+done <files-to-copy.txt
+
+echo "Synching ..."
+rsync -avzr dist/* ${HOST}:${DEST_DIR}
